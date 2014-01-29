c46f273cda4a189756c5104c504feddacce8b4a2
skylarjhdownes <skylarjhdownes@gmail.com>
2014-01-29 11:39:09 -0700

    Added Token enum.

--- microPascalScanner.java
+++ microPascalScanner.java
@@ -4,6 +4,29 @@ import java.util.Scanner;
 import java.io.*;
 
 public class microPascalScanner {
+	public enum Token 
+	{
+		//Reserved words:
+	    MP_AND, MP_BEGIN, MP_DIV, MP_DO, MP_DOWNTO, MP_ELSE,
+	    MP_END, MP_FIXED, MP_FLOAT, MP_FOR, MP_FUNCTION, MP_IF,
+	    MP_INTEGER, MP_MOD, MP_NOT, MP_OR, MP_PROCEDURE, 
+	    MP_PROGRAM, MP_READ, MP_REPEAT, MP_THEN, MP_TO, MP_UNTIL, 
+	    MP_VAR, MP_WHILE, MP_WRITE,
+	    
+	    //Identifiers and literals:
+	    MP_IDENTIFIER, MP_INTEGER_LIT, MP_FIXED_LIT, MP_FLOAT_LIT , MP_STRING_LIT, 
+	    
+	    //Symbols:
+	    MP_PERIOD, MP_COMMA, MP_SCOLON, MP_LPAREN, MP_RPAREN, 
+	    MP_EQUAL, MP_GTHAN, MP_GEQUAL, MP_LTHAN, MP_LEQUAL, 
+	    MP_NEQUAL, MP_ASSIGN, MP_PLUS, MP_MINUS, MP_TIMES, MP_COLON,
+	    
+	    //End of file:
+	    MP_EOF,
+	    
+	    //Errors:
+	    MP_RUN_COMMENT, MP_RUN_STRING, MP_ERROR
+	}
 	Scanner scanner;
 	String lexeme;
 	int lineNumber;
@@ -13,7 +36,7 @@ public class microPascalScanner {
 	{
 		try
 		{
-			scanner = new Scanner(new BufferedReader(new FileReader("xanadu.txt")));
+			scanner = new Scanner(new BufferedReader(new FileReader("source-code-file.pas")));
 		}
 		catch(FileNotFoundException e)
 		{
