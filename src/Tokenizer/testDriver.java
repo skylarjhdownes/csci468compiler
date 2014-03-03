c66affcfc1c1489c79e97fc560cf35c3d093d85c
skylarjhdownes <skylarjhdownes@gmail.com>
2014-03-02 17:32:29 -0700

    Made the driver fail more, er... gracefully if there is no test file.

--- src/Tokenizer/testDriver.java
+++ src/Tokenizer/testDriver.java
@@ -2,17 +2,25 @@ package Tokenizer;
 
 import java.io.File;
 
-public class testDriver {
-
-	public static void main(String[] args) {
+public class testDriver 
+{
+	public static void main(String[] args) throws java.io.IOException
+	{
 		File testFile;
 
 		if( args.length == 0 )
 		{
 			System.out.println("No input file given, ALL IS LOST. ABANDON SHIP!!!!!!!!!!!!!!!!");
-			
-			testFile = new File("C:\\Users\\Stephen Bush\\Documents\\Test.txt");
-
+			System.out.println("Unless, perhaps, we have a local test file?  Let's see...");
+			testFile = new File("src\\Test.txt");
+			if (testFile.isFile())
+			{
+				System.out.println("Hey!  There's a test file.  Let's use that.");
+			}
+			else
+			{
+				System.out.println("NOOOOOOPE!  There is no test file at src\\Test.txt.  RUN FOR THE HILLS!");
+			}
 			MicroPascalScanner sc = new MicroPascalScanner(testFile);
 
 			Token current;
