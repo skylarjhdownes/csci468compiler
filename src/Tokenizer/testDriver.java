2c34bd3930aee52fb62976977e04b9d25e7dd179
skylarjhdownes <skylarjhdownes@gmail.com>
2014-03-02 21:26:44 -0700

    Working on a user choice thing for test files.

--- src/Tokenizer/testDriver.java
+++ src/Tokenizer/testDriver.java
@@ -1,26 +1,45 @@
 package Tokenizer;
 
-import java.io.File;
+import java.io.*;
+import java.util.Scanner;
 
 public class testDriver 
 {
+	private static Scanner reader;
+
 	public static void main(String[] args) throws java.io.IOException
 	{
 		File testFile;
-
+		reader = new Scanner(System.in);
+		
 		if( args.length == 0 )
 		{
+			File folder = new File("TestPrograms");
+			File[] listOfFiles = folder.listFiles();
+		    
 			System.out.println("No input file given, ALL IS LOST. ABANDON SHIP!!!!!!!!!!!!!!!!");
-			System.out.println("Unless, perhaps, we have a local test file?  Let's see...");
-			testFile = new File("src\\Test.txt");
-			if (testFile.isFile())
-			{
-				System.out.println("Hey!  There's a test file.  Let's use that.");
-			}
-			else
-			{
-				System.out.println("NOOOOOOPE!  There is no test file at src\\Test.txt.  RUN FOR THE HILLS!");
-			}
+			System.out.println("Actually, lets use a local test file.  Let's see...");
+			
+			
+			System.out.println("Try picking from this list:");
+			for (int i = 0; i < listOfFiles.length; i++) 
+		    {
+		    	if (listOfFiles[i].isFile()) {
+		    		System.out.println(i + ": " + listOfFiles[i].getName());
+		    	}
+		    }
+			
+			System.out.print ("Enter an integer:");
+			
+//			boolean goodInput;
+//			while(!goodInput)
+//			{
+				int userChoice = reader.nextInt();
+				
+//			}
+		    
+			testFile = new File("TestPrograms\\" + listOfFiles[userChoice].getName());
+			
 			MicroPascalScanner sc = new MicroPascalScanner(testFile);
 
 			Token current;
